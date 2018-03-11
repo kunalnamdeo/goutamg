2b4be21593aae35daf40b5da72b53fb8fae03a07
Kunal Namdeo <kunal.scjp@gmail.com>
2018-03-11 14:11:08 +0530

    commit

--- src/goutamg/kanwiz/util/DBConnection.java
+++ src/goutamg/kanwiz/util/DBConnection.java
@@ -12,8 +12,7 @@ public class DBConnection {
 		    Connection con = DriverManager.getConnection(  "jdbc:mysql://localhost:3306/test","root",""); 
 		   
 		   // Prod
-		   //Connection con = DriverManager.getConnection(  "jdbc:mysql://localhost:3306/goutamgh_test","goutamgh_admin","kanwiz@1981"); 
-		   
+		  
 		   return con;
 	}
 
